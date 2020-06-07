149f22726076124775c7c649d1e7ce5eb04e0c6f
felipe Januario <felipejanuario86@gmail.com>
2020-06-06 21:54:30 -0300

    Cadastrando pontos de coleta a partir do formulário

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState,ChangeEvent} from 'react';
-import {Link} from 'react-router-dom';
+import React, {useEffect, useState,ChangeEvent, FormEvent} from 'react';
+import {Link,useHistory} from 'react-router-dom';
 import {FiArrowLeft} from 'react-icons/fi';
 import {Map,TileLayer,Marker} from 'react-leaflet';
 import axios from 'axios';
@@ -43,6 +43,8 @@ const CreatePoint = () =>{
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
     const [selectedPosition,setSelectedPosition] = useState<[number,number]>([0,0]);
 
+
+    const history = useHistory();
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(position =>{
@@ -119,6 +121,34 @@ const CreatePoint = () =>{
         }       
     }
 
+  async function handleSubmit(event: FormEvent){
+        event.preventDefault();
+
+        const {name, email,whatsapp} = formData;
+        const uf = selectedUf;
+        const city = selectedCity;
+        const [latitude, longitude] = selectedPosition;
+        const items = selectedItems;
+
+        const data ={
+            name,
+            email,
+            whatsapp,
+            uf,
+            city,
+            latitude,
+            longitude,
+            items
+        };
+
+        await api.post('points', data);
+
+        alert('Ponto de coleta Criado');
+
+        history.push('/');
+
+    }
+
     return (
         <div id="page-create-point">
             <header>
@@ -130,7 +160,7 @@ const CreatePoint = () =>{
                 </Link>
             </header>
 
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <h1>Cadastro do <br/> ponto de coleta</h1>
                 
                 <fieldset>
